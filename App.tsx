92f6c94316fc55d6593a4fabd712d2b1e9885037
unknown <munyaarmel61@gmail.com>
2023-05-30 13:39:51 +0200

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ScrollView,TextInput } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Welcome to the ASAP Delivery</Text>
+      <Text>Sign in</Text>
+      <View>
+        <Text style={styles.phoneLabel}>phone number:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="e.g:+250785679625"
+          keyboardType="numeric"
+        />
+      </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 0.3,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  phoneLabel: {
+    
+  }
 });
